ba07a3cd90d9156086ad6c5c82e64dd1babe2180
Lenovo <yash.radadiya1088@gmail.com>
2020-04-08 14:03:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/wcmc_praticals/fragment1.java
@@ -0,0 +1,25 @@
+package com.example.wcmc_praticals;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class fragment1 extends android.app.Fragment {
+    ListView simpleList;
+    String countryList[] = {"Dhoni","De-Villiers","Virat","Rohit","Warner"};
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment1, container, false);
+
+
+        simpleList = (ListView)view.findViewById(R.id.simpleListView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(view.getContext(), R.layout.activity_listview, R.id.textView, countryList);
+        simpleList.setAdapter(arrayAdapter);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/wcmc_praticals/fragment2.java
@@ -0,0 +1,47 @@
+package com.example.wcmc_praticals;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class fragment2 extends android.app.Fragment {
+    CardView simpleList;
+    String countryList[] = {"Dhoni","De-Villiers","Virat","Rohit","Warner"};
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private static String LOG_TAG = "CardViewActivity";
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment2, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(view.getContext(),LinearLayoutManager.HORIZONTAL,false);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new MyRecyclerViewAdapter(getDataSet());
+        mRecyclerView.setAdapter(mAdapter);
+
+        return view;
+    }
+
+    private ArrayList<DataObject> getDataSet() {
+        ArrayList results = new ArrayList<DataObject>();
+        for (int index = 0; index < 5; index++) {
+            DataObject obj = new DataObject(countryList[index],
+                    "Secondary " + index);
+            results.add(index, obj);
+        }
+        return results;
+    }
+}
